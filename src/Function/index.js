6646de4e1edf5a6836a22921904f47613fccedcb
sydneyincolor <75862203+sydneyincolor@users.noreply.github.com>
2021-04-25 05:46:53 -0500

    Update index.js

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,6 +1,18 @@
+const cfnResponse = require('cfn-custom-resource');
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
+  
+  try{
+    console.log('We succeeded');
+    
+    cfnReponse.sendSuccess('PopulateFrontendContent',{},event);
+  } catch (err){
+    console.error(err);
+    
+    await cfnResponse.sendFailure(err.message, event);
+  }
 
   return {};
 };
